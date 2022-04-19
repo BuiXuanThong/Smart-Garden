a99af86a491e4c3f141bf8751c75c26fc101e1c0
Bùi Xuân Thông <akirakuma5@gmail.com>
2022-04-19 22:55:33 +0700

    add led, pump btn

--- src/layouts/tables/data/authorsTableData.js
+++ src/layouts/tables/data/authorsTableData.js
@@ -19,14 +19,79 @@ Coded by www.creative-tim.com
 import MDBox from "components/MDBox";
 import MDTypography from "components/MDTypography";
 import MDAvatar from "components/MDAvatar";
-import MDBadge from "components/MDBadge";
+// import MDBadge from "components/MDBadge";
 
 // Images
-import team2 from "assets/images/team-2.jpg";
-import team3 from "assets/images/team-3.jpg";
-import team4 from "assets/images/team-4.jpg";
+import ledIcon from "assets/images/led.png";
+import pumpIcon from "assets/images/pump.jpg";
+
+// useState
+import { useState, useEffect } from "react";
+
+
+// css
+import "./style.css";
+
+
+const fs = "firstState";
+
 
 export default function data() {
+  const [ledState, setLedState] = useState(fs);
+  const [pumpState, setPumpState] = useState(fs);
+
+  function ledChangeToState(state) {
+    const value = (state? "1" : "0");
+
+    fetch("https://io.adafruit.com/api/v2/trong249/feeds/bbc-led/data", {
+
+    	// Adding method type
+    	method: "POST",
+
+    	// Adding body or contents to send
+    	body: JSON.stringify({
+    		"X-AIO-Key": "aio_nnDo095XgBmSBw6aM5tr84OVjvoY",
+        	"datum":{"value": value}
+    	}),
+
+    	// Adding headers to the request
+    	headers: {
+    		"Content-type": "application/json; charset=UTF-8"
+    	}
+    })
+  }
+
+  function pumpChangeToState(state) {
+      const value = (state? "3" : "2");
+
+      fetch("https://io.adafruit.com/api/v2/trong249/feeds/bbc-pump/data", {
+
+        // Adding method type
+        method: "POST",
+
+        // Adding body or contents to send
+        body: JSON.stringify({
+          "X-AIO-Key": "aio_nnDo095XgBmSBw6aM5tr84OVjvoY",
+            "datum":{"value": value}
+        }),
+
+        // Adding headers to the request
+        headers: {
+          "Content-type": "application/json; charset=UTF-8"
+        }
+      })
+  }
+
+  useEffect(() => {
+    setInterval(() => {
+      fetch(`https://io.adafruit.com/api/v2/trong249/feeds/bbc-led/data`).then((response) => response.json())
+        .then((actualData) => setLedState(actualData[0].value === "1" ? true : false));
+
+      fetch(`https://io.adafruit.com/api/v2/trong249/feeds/bbc-pump/data`).then((response) => response.json())
+        .then((actualData) => setPumpState(actualData[0].value === "3" ? true : false));
+    }, 500);
+  }, []);
+
   const Author = ({ image, name, email }) => (
     <MDBox display="flex" alignItems="center" lineHeight={1}>
       <MDAvatar src={image} name={name} size="sm" />
@@ -51,26 +116,32 @@ export default function data() {
   return {
     columns: [
       { Header: "author", accessor: "author", width: "45%", align: "left" },
-      { Header: "function", accessor: "function", align: "left" },
+      { Header: "Current", accessor: "function", align: "left" },
       { Header: "status", accessor: "status", align: "center" },
-      { Header: "employed", accessor: "employed", align: "center" },
+      // { Header: "employed", accessor: "employed", align: "center" },
       { Header: "action", accessor: "action", align: "center" },
     ],
 
     rows: [
       {
-        author: <Author image={team2} name="John Michael" email="john@creative-tim.com" />,
-        function: <Job title="Manager" description="Organization" />,
+        author: <Author image={ledIcon} name="LED BUTTON" email="" />,
+        function: <Job title={ledState !== fs ? "Updated" : "Loading"} description="" />,
         status: (
           <MDBox ml={-1}>
-            <MDBadge badgeContent="online" color="success" variant="gradient" size="sm" />
+            <label className="switch" htmlFor="led-btn">
+                <input id="led-btn"
+                  type="checkbox"
+                  checked={ledState}
+                  onClick={() => {ledChangeToState(!ledState)}} />
+                <span className="slider round"/>
+            </label>
           </MDBox>
         ),
-        employed: (
-          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
-            23/04/18
-          </MDTypography>
-        ),
+        // employed: (
+        //   <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+        //     23/04/18
+        //   </MDTypography>
+        // ),
         action: (
           <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
             Edit
@@ -78,92 +149,22 @@ export default function data() {
         ),
       },
       {
-        author: <Author image={team3} name="Alexa Liras" email="alexa@creative-tim.com" />,
-        function: <Job title="Programator" description="Developer" />,
+        author: <Author image={pumpIcon} name="PUMP BUTTON" email="" />,
+        function: <Job title={pumpState !== fs ? "Updated" : "Loading"} description="" />,
         status: (
           <MDBox ml={-1}>
-            <MDBadge badgeContent="offline" color="dark" variant="gradient" size="sm" />
+            <label className="switch" htmlFor="pump-btn">
+                <input id="pump-btn"
+                type="checkbox"
+                checked={pumpState}
+                onClick={() => {pumpChangeToState(!pumpState)}}/>
+                <span className="slider round"/>
+            </label>
           </MDBox>
         ),
         employed: (
           <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
             11/01/19
-          </MDTypography>
-        ),
-        action: (
-          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
-            Edit
-          </MDTypography>
-        ),
-      },
-      {
-        author: <Author image={team4} name="Laurent Perrier" email="laurent@creative-tim.com" />,
-        function: <Job title="Executive" description="Projects" />,
-        status: (
-          <MDBox ml={-1}>
-            <MDBadge badgeContent="online" color="success" variant="gradient" size="sm" />
-          </MDBox>
-        ),
-        employed: (
-          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
-            19/09/17
-          </MDTypography>
-        ),
-        action: (
-          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
-            Edit
-          </MDTypography>
-        ),
-      },
-      {
-        author: <Author image={team3} name="Michael Levi" email="michael@creative-tim.com" />,
-        function: <Job title="Programator" description="Developer" />,
-        status: (
-          <MDBox ml={-1}>
-            <MDBadge badgeContent="online" color="success" variant="gradient" size="sm" />
-          </MDBox>
-        ),
-        employed: (
-          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
-            24/12/08
-          </MDTypography>
-        ),
-        action: (
-          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
-            Edit
-          </MDTypography>
-        ),
-      },
-      {
-        author: <Author image={team3} name="Richard Gran" email="richard@creative-tim.com" />,
-        function: <Job title="Manager" description="Executive" />,
-        status: (
-          <MDBox ml={-1}>
-            <MDBadge badgeContent="offline" color="dark" variant="gradient" size="sm" />
-          </MDBox>
-        ),
-        employed: (
-          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
-            04/10/21
-          </MDTypography>
-        ),
-        action: (
-          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
-            Edit
-          </MDTypography>
-        ),
-      },
-      {
-        author: <Author image={team4} name="Miriam Eric" email="miriam@creative-tim.com" />,
-        function: <Job title="Programator" description="Developer" />,
-        status: (
-          <MDBox ml={-1}>
-            <MDBadge badgeContent="offline" color="dark" variant="gradient" size="sm" />
-          </MDBox>
-        ),
-        employed: (
-          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
-            14/09/20
           </MDTypography>
         ),
         action: (
