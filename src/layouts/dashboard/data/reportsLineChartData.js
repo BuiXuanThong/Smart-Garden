7180469b59559ce52fda59422e2614b8bc87590a
than2k1 <phamngocthan2001@gmail.com>
2022-05-03 22:50:56 +0700

    dashboard adjust

--- src/layouts/dashboard/data/reportsLineChartData.js
+++ src/layouts/dashboard/data/reportsLineChartData.js
@@ -1,6 +1,6 @@
 export default {
   tasks: {
-    labels: [1, 2, 3, 4, 5],
-    datasets: { label: "Temperature", data: [1, 2, 3, 4, 5] },
+    labels: [],
+    datasets: { label: "", data: [] },
   },
 };
--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -44,23 +44,55 @@ const Dashboard = () => {
   const [light, setLight] = useState();
   const [humiGr, setHumiGr] = useState();
 
-  const { tasks } = reportsLineChartData;
+  const [tempChartData, setTempChartData] = useState(reportsLineChartData);
+  const [lightChartData, setLightChartData] = useState(reportsLineChartData);
+  const [humidChartData, setHumidChartData] = useState(reportsLineChartData);
+  const [humidGrChartData, setHumidGrChartData] = useState(reportsLineChartData);
+  
 
+  const baseUrl = "https://express-json.herokuapp.com";
 
   useEffect(() => {
     setInterval(() => {
-      fetch(`https://io.adafruit.com/api/v2/trong249/feeds/bbc-temp/data`).then((response) => response.json())
-        .then((actualData) => setClockState(actualData[0].value));
+      // fetch(`https://io.adafruit.com/api/v2/trong249/feeds/bbc-temp/data`).then((response) => response.json())
+      //   .then((actualData) => setClockState(actualData[0].value));
 
-      fetch(`https://io.adafruit.com/api/v2/trong249/feeds/bbc-humi/data`).then((response) => response.json())
-        .then((actualData) => setHumid(actualData[0].value));
+      // fetch(`https://io.adafruit.com/api/v2/trong249/feeds/bbc-humi/data`).then((response) => response.json())
+      //   .then((actualData) => setHumid(actualData[0].value));
 
-      fetch(`https://io.adafruit.com/api/v2/trong249/feeds/bbc-light/data`).then((response) => response.json())
-        .then((actualData) => setLight(actualData[0].value));
+      // fetch(`https://io.adafruit.com/api/v2/trong249/feeds/bbc-light/data`).then((response) => response.json())
+      //   .then((actualData) => setLight(actualData[0].value));
 
-      fetch(`https://io.adafruit.com/api/v2/trong249/feeds/bbc-humi-ground/data`).then((response) => response.json())
-        .then((actualData) => setHumiGr(actualData[0].value));
+      // fetch(`https://io.adafruit.com/api/v2/trong249/feeds/bbc-humi-ground/data`).then((response) => response.json())
+      //   .then((actualData) => setHumiGr(actualData[0].value));
+      fetch(baseUrl+"/dashboard/data/cards").then((response) => response.json())
+      .then((actualData) => {
+        setClockState(actualData.tempValue);
+        setHumid(actualData.humidValue);
+        setLight(actualData.lightValue);
+        setHumiGr(actualData.grHumidValue);
+      })
     }, 1000);
+
+    fetch(baseUrl+"/dashboard/data/temp-chart/3000").then((response) => response.json())
+      .then((actualData) => {
+        setTempChartData(actualData);
+      })
+
+      fetch(baseUrl+"/dashboard/data/humid-chart/3000").then((response) => response.json())
+      .then((actualData) => {
+        setHumidChartData(actualData);
+      })
+
+      fetch(baseUrl+"/dashboard/data/light-chart/3000").then((response) => response.json())
+      .then((actualData) =>{
+        setLightChartData(actualData);
+      })
+
+      fetch(baseUrl+"/dashboard/data/humid-ground-chart/3000").then((response) => response.json())
+      .then((actualData) => {
+        setHumidGrChartData(actualData);
+      })
   }, []);
 
   return (
@@ -132,14 +164,57 @@ const Dashboard = () => {
 
       <MDBox mt={4.5}>
           <Grid container spacing={3}>
-            <Grid item xs={12} md={6} lg={4}>
-              <MDBox mb={3}>
+            <Grid item xs={12} md={8} lg={6}>
+              {/* <MDBox mb={3}>
                 <ReportsLineChart
                   color="dark"
                   title="Temperature"
                   description=""
                   date="just updated"
                   chart={tasks}
+                />
+              </MDBox> */}
+
+              <MDBox mb={3}>
+                <ReportsLineChart
+                  color="dark"
+                  title="Temperature"
+                  description=""
+                  date="just updated"
+                  chart={tempChartData}
+                />
+              </MDBox>
+              </Grid>
+              <Grid item xs={12} md={8} lg={6}>
+              <MDBox mb={3}>
+                <ReportsLineChart
+                  color="dark"
+                  title="Humid"
+                  description=""
+                  date="just updated"
+                  chart={humidChartData}
+                />
+              </MDBox>
+              </Grid>
+              <Grid item xs={12} md={8} lg={6}>
+              <MDBox mb={3}>
+                <ReportsLineChart
+                  color="dark"
+                  title="Light"
+                  description=""
+                  date="just updated"
+                  chart={lightChartData}
+                />
+              </MDBox>
+              </Grid>
+              <Grid item xs={12} md={8} lg={6}>
+              <MDBox mb={3}>
+                <ReportsLineChart
+                  color="dark"
+                  title="Ground Humid"
+                  description=""
+                  date="just updated"
+                  chart={humidGrChartData}
                 />
               </MDBox>
             </Grid>
